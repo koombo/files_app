9b7c92c0ae1911184efbdca7159cd02a39a079eb
koombo <surmiak.karolina@gmail.com>
2022-12-08 15:03:04 +0100

    change index to show current user docs

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_08_123657) do
+ActiveRecord::Schema.define(version: 2022_12_08_124315) do
 
   create_table "docs", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
